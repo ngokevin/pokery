c3b0f4e5f7cfef68f997af48a65ba958acd76935
Kevin Ngo <me@ngokevin.com>
2015-07-27 00:28:42 -0700

    bugfix simulator, but too many ties

--- src/classes/Simulator.js
+++ src/classes/Simulator.js
@@ -3,16 +3,16 @@
 */
 import _ from 'lodash';
 
+import c from '../constants';
 import Deck from './Deck';
 import Hand from './Hand';
 import HoleCards from './HoleCards';
 
 
 export default class Simulator {
-  constructor(hands, board) {
+  constructor(hands=[], board=[]) {
     // hands -- ['AhCd', QQ, 'AKs'].
     // board -- ['Ah', Jd', 2s'].
-
     this.hands = hands.map(hand => new HoleCards(hand));
     this.board = board;
 
@@ -35,27 +35,11 @@ export default class Simulator {
 
       // Build the full 7-card hands.
       let madeHands = hands.map(hand => new Hand(hand.concat(board)));
-
-      // Find the winning hand by looping over and comparing to the previous.
-      let winningHand;
-      madeHands.forEach((hand, i) => {
-        if (i === 0) {
-          // Nothing to compare.
-          return;
-        }
-        const result = hand.vs(madeHands[i - 1]);
-        if (result === -1 && !winningHand) {
-          // If the first hand won, set it as the winning hand.
-          winningHand = i - 1;
-        } else if (result === 1) {
-          // New winning hand.
-          winningHand = i;
-        }
-      });
+      let winningHand = maxHand(madeHands);
 
       this.runs++;
 
-      if (winningHand !== undefined) {
+      if (winningHand !== -1) {
         this.results[winningHand].wins++;
       } else {
         this.ties++;
@@ -65,13 +49,12 @@ export default class Simulator {
   getResults() {
     // Aggregate results.
     return this.results.map(result => {
-      const wins = result.wins;
-      const losses = this.runs - result.wins;
-      const ties = this.ties;
       const runs = this.runs;
+      const wins = result.wins;
+      const ties = this.ties;
+      const losses = runs - result.wins - ties;
 
       return _.extend(result, {
-        // equity = win% + (tie% / #hands).
         losses: losses,
         ties: ties,
 
@@ -79,8 +62,39 @@ export default class Simulator {
         tiePct: (ties / runs) * 100,
         winPct: (wins / runs) * 100,
 
+        // equity = win% + (tie% / #hands).
         ev: (wins / runs) + (ties / runs / this.results.length),
       });
     });
   }
+}
+
+
+export function maxHand(hands) {
+  /*
+    Returns the index of the best hand, -1 if all a tie.
+    hands -- List of hands (e.g., [[Ac,Ad,Ah,As,Kc,Kd,Kh]]).
+  */
+  // Find the winning hand by looping over and comparing to the previous.
+  let winningHand;
+
+  hands.forEach((hand, i) => {
+    if (i === 0) {
+      // Nothing to compare.
+      return;
+    }
+
+    let handMatchup = winningHand === undefined ? i - 1 : winningHand;
+    let result = hand.vs(hands[handMatchup]);
+
+    if (result === -1 && winningHand === undefined) {
+      // If the first hand won, set it as the winning hand.
+      winningHand = i - 1;
+    } else if (result === 1) {
+      // New winning hand.
+      winningHand = i;
+    }
+  });
+
+  return winningHand !== undefined ? winningHand : -1;
 }
